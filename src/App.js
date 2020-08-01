cf643928da15568a22fb0b9cefb65ae239e90227
SaurabhApraj <saurabhapraj7@gmail.com>
2020-08-01 19:11:53 +0530

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import React, { createContext } from "react";
+import React, { createContext, useState, useEffect } from "react";
 import CompoA from "./components/CompoA";
 
 const FirstName = createContext();
 const LastName = createContext();
 
 const App = () => {
+  const [num, setNum] = useState(0);
+  const [nums, setNums] = useState(0);
+
+  useEffect(() => {
+    alert("clicked");
+  }, [num]);
+
   return (
     <>
       <FirstName.Provider value={"Saurabh"}>
@@ -12,6 +19,22 @@ const App = () => {
           <CompoA />
         </LastName.Provider>
       </FirstName.Provider>
+
+      <button
+        onClick={() => {
+          setNum(num + 1);
+        }}
+      >
+        Click {num}
+      </button>
+      <br />
+      <button
+        onClick={() => {
+          setNums(nums + 1);
+        }}
+      >
+        Click {nums}
+      </button>
     </>
   );
 };
